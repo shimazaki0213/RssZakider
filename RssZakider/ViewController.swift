5e835e4657ff04b61a18d110aace74f46b276c0b
島崎貴弘 <shimazakitakahiro@shimazakitakahiro-no-macbook-air.local>
2018-11-06 20:47:10 +0900

    Initial Commit

---
+++ RssZakider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RssZakider
+//
+//  Created by 島崎貴弘 on 2018/11/06.
+//  Copyright © 2018年 島崎貴弘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
